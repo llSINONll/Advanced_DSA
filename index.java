1ff6f646296c568054ab6ef1a50e2866e311d499
llSINONll <sinon6264@gmail.com>
2025-07-02 22:50:45 +0530

    Added the day 1 code

---
+++ index.java
@@ -0,0 +1,52 @@
+
+
+
+// public class index {
+//   public static void main(String[] args) {
+//     // System.out.println("Hello");
+//     // for(int i=0; i<5; i++){
+//     //   System.out.println(i);
+//     // }
+//     // int[] arr={1,2,3,4,5};
+//     // for (int i=arr.length-1; i>=0; i--){
+//     //   System.out.println(arr[i]);
+//     // }
+
+//     //#1 Max value of the array
+//     // int[] arr={3,2,8,6};
+//     // int max=arr[0];
+//     // for (int i=0; i<arr.length; i++){
+//     //   if(arr[i]>max){
+//     //     max=arr[i];
+//     //   }
+//     // }
+//     // System.out.println(max);
+
+//     //#2 Sum of the array
+//     // int[] arr={3,5,2,6,7,3,7};
+//     // int sum = 0;
+//     // for (int i=0; i<arr.length; i++){
+//     //   sum+=arr[i];
+//     // }
+//     // System.out.println(sum);
+
+
+//   }
+  
+// 
+// import java.util.Scanner;
+// public class index{
+//   public static void main(String[] args) {
+//     Scanner in =new Scanner(System.in);
+//       System.out.println("Enter size of the array");
+//       int n= in.nextInt();
+//       int[] arr=new int[n];
+//       for (int i = 0; i<n; i++) {
+//         System.out.println("Enter the Element: ");
+//         arr[i]=in.nextInt();
+//       }
+//       for(int k=0; k<arr.length; k++){
+//         System.out.println(arr[k] +"   "+"elem");
+//       }
+//   }
+// }
